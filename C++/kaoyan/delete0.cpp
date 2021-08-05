f1a4b005c1a25b60362ab28f1503433ad7338680
山水夜止 <2270517836@qq.com>
2021-08-05 22:41:22 +0800

    新仓库测试

--- C++/kaoyan/delete0.cpp
+++ C++/kaoyan/delete0.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 /*
  * 考研中的顺序表删除
- * 熟悉一下
+ * 熟悉一下语法
  */
 
 typedef struct List {
@@ -11,17 +11,19 @@ typedef struct List {
     int length;
 } List;
 
-int delete(List *list, int site);
+int delete0(List *&list, int site, int &x);
 
-int delete(List *list, int site) {
+int delete0(List *&list, int site, int &x) {
     if (site < 0 || site > list->length) {
         return 0;
     }
     int i = site;
+    x = list->array[i];
     for (i; i < list->length - 1; i++) {
         list->array[i] = list->array[i + 1];
     }
     list->length = list->length - 1;
+
 }
 int main() {
     List *list = (List*)malloc(sizeof(List));
@@ -30,8 +32,10 @@ int main() {
     list->array[2] = 2;
     list->array[3] = 4;
     list->length = 4;
-    delete(list, 2);
+    int x;
+    delete0(list, 2, x);
     for (int i = 0; i < 4; i++) {
         printf("array[%d]: %d\n", i, list->array[i]);
     }
+    printf("%d", x);
 }
